be5eff7c7d4f558e8b40a5f40674d1c3c953b91b
Jessica Pamdeth <59935702+jpamdeth@users.noreply.github.com>
2024-02-24 17:30:24 -0500

    Add service and function docs

--- src/library/library.service.ts
+++ src/library/library.service.ts
@@ -5,6 +5,9 @@ import { ChatCompletionMessageParam } from 'openai/resources';
 import { Author, Book, Genre } from './library.models';
 import { AuthorDto, BookDto, GenreDto } from './library.dto';
 
+/**
+ * Service class for managing library operations.
+ */
 @Injectable()
 export class LibraryService {
   private readonly logger = new Logger(LibraryService.name);
@@ -14,11 +17,20 @@ export class LibraryService {
     private openai: OpenAIService,
   ) {}
 
+  /**
+   * Retrieves all authors from the library.
+   * @returns A promise that resolves to an array of authors.
+   */
   async getAuthors(): Promise<Author[]> {
     this.logger.debug('Getting all authors');
     return this.prisma.author.findMany();
   }
 
+  /**
+   * Creates a new author in the library.
+   * @param author - The author data to be created.
+   * @returns A promise that resolves to the created author.
+   */
   async createAuthor(author: AuthorDto): Promise<Author> {
     this.logger.debug(
       'Saving author ' + author.firstName + ' ' + author.lastName,
@@ -26,6 +38,12 @@ export class LibraryService {
     return this.prisma.author.create({ data: author });
   }
 
+  /**
+   * Updates an existing author in the library.
+   * @param author - The updated author data.
+   * @param authorId - The ID of the author to be updated.
+   * @returns A promise that resolves to the updated author.
+   */
   async updateAuthor(author: AuthorDto, authorId: string): Promise<Author> {
     this.logger.debug('Updating author ' + authorId);
     return this.prisma.author.update({
@@ -34,11 +52,22 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Creates a new genre in the library.
+   * @param genre - The genre data to be created.
+   * @returns A promise that resolves to the created genre.
+   */
   async createGenre(genre: GenreDto): Promise<Genre> {
     this.logger.debug('Saving genre ' + genre.genre);
     return this.prisma.genre.create({ data: genre });
   }
 
+  /**
+   * Updates an existing genre in the library.
+   * @param genre - The updated genre data.
+   * @param genreId - The ID of the genre to be updated.
+   * @returns A promise that resolves to the updated genre.
+   */
   async updateGenre(genre: GenreDto, genreId: string): Promise<Genre> {
     this.logger.debug('Updating genre ' + genreId);
     return this.prisma.genre.update({
@@ -47,11 +76,22 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Creates a new book in the library.
+   * @param book - The book data to be created.
+   * @returns A promise that resolves to the created book.
+   */
   async createBook(book: BookDto): Promise<Book> {
     this.logger.debug('Saving book ' + book.title);
     return this.prisma.book.create({ data: book });
   }
 
+  /**
+   * Updates an existing book in the library.
+   * @param book - The updated book data.
+   * @param bookId - The ID of the book to be updated.
+   * @returns A promise that resolves to the updated book.
+   */
   async updateBook(book: BookDto, bookId: string): Promise<Book> {
     this.logger.debug('Updating book ' + bookId);
     return this.prisma.book.update({
@@ -60,6 +100,11 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Retrieves an author from the library by ID.
+   * @param authorId - The ID of the author to retrieve.
+   * @returns A promise that resolves to the retrieved author.
+   */
   async getAuthor(authorId: string): Promise<Author> {
     this.logger.debug('Getting author ' + authorId);
     return this.prisma.author.findUnique({
@@ -74,6 +119,11 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Retrieves a genre from the library by ID.
+   * @param genreId - The ID of the genre to retrieve.
+   * @returns A promise that resolves to the retrieved genre.
+   */
   async getGenre(genreId: string): Promise<Genre> {
     this.logger.debug('Getting genre ' + genreId);
     return this.prisma.genre.findUnique({
@@ -81,6 +131,11 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Retrieves a book from the library by ID.
+   * @param bookId - The ID of the book to retrieve.
+   * @returns A promise that resolves to the retrieved book.
+   */
   async getBook(bookId: string): Promise<Book> {
     this.logger.debug('Getting book ' + bookId);
     return this.prisma.book.findUnique({
@@ -88,6 +143,11 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Deletes an author from the library by ID.
+   * @param authorId - The ID of the author to delete.
+   * @returns A promise that resolves to the deleted author.
+   */
   async deleteAuthor(authorId: string): Promise<Author> {
     this.logger.debug('Deleting author ' + authorId);
     return this.prisma.author.delete({
@@ -95,6 +155,11 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Deletes a genre from the library by ID.
+   * @param genreId - The ID of the genre to delete.
+   * @returns A promise that resolves to the deleted genre.
+   */
   async deleteGenre(genreId: string): Promise<Genre> {
     this.logger.debug('Deleting genre ' + genreId);
     return this.prisma.genre.delete({
@@ -102,6 +167,11 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Deletes a book from the library by ID.
+   * @param bookId - The ID of the book to delete.
+   * @returns A promise that resolves to the deleted book.
+   */
   async deleteBook(bookId: string): Promise<Book> {
     this.logger.debug('Deleting book ' + bookId);
     return this.prisma.book.delete({
@@ -109,6 +179,13 @@ export class LibraryService {
     });
   }
 
+  /**
+   * Creates an author and suggests books for the author.
+   * @param firstName - The first name of the author.
+   * @param lastName - The last name of the author.
+   * @param apiKey - The API key for accessing OpenAI APIs.
+   * @returns A promise that resolves to the suggested books in JSON format.
+   */
   async createAndSuggestBooks(
     firstName: string,
     lastName: string,
@@ -118,6 +195,12 @@ export class LibraryService {
     return this.suggestBooks(author.id, apiKey);
   }
 
+  /**
+   * Suggests books for an author.
+   * @param authorId - The ID of the author.
+   * @param apiKey - The API key for accessing OpenAI APIs.
+   * @returns A promise that resolves to the suggested books in JSON format.
+   */
   async suggestBooks(authorId: string, apiKey: string): Promise<string> {
     this.logger.debug('Suggesting books for author ' + authorId);
 
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -3,10 +3,19 @@ import { OpenAI } from 'openai';
 import { ChatCompletion, ChatCompletionMessageParam } from 'openai/resources';
 
 @Injectable()
+/**
+ * Service class for interacting with the OpenAI API.
+ */
 export class OpenAIService {
   private readonly logger = new Logger(OpenAIService.name);
   private openai: OpenAI;
 
+  /**
+   * Retrieves suggestions from the OpenAI API.
+   * @param messages - An array of chat completion messages.
+   * @param apiKey - The API key for accessing the OpenAI API.
+   * @returns A promise that resolves to a string representing the suggested completion.
+   */
   async getSuggestions(
     messages: ChatCompletionMessageParam[],
     apiKey: string,
